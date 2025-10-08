60ff9dd88a3dbb98945eabf8e6112e7fdfffc8b7
ryanblyth <ryanjblyth@gmail.com>
2025-10-07 18:12:22 -0600

    feat: add styles for ice shelves and glaciated areas to go along with ice pmtile

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,5 @@
 /* global maplibregl, pmtiles */
 
-// import * as pmtiles from "pmtiles";
-// import maplibregl from "maplibre-gl";
-
 // Register PMTiles BEFORE creating the map
 const protocol = new pmtiles.Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
@@ -10,25 +7,35 @@ maplibregl.addProtocol("pmtiles", protocol.tile);
 const map = new maplibregl.Map({
   container: "map",
   style: "/styles/basemap-midieval.json",
-  center: [0, 0], // World center
-  zoom: 2,     // World view
-  hash: false  // disable hash to prevent URL overrides
+  center: [-105.7821, 39.5501],
+  zoom: 8,
+  // remove maxZoom so we can overzoom
+  hash: false
 });
 
 map.addControl(new maplibregl.NavigationControl(), "top-right");
 
+// Uncap any layer maxzoom so overzoom works >15
+map.on("styledata", () => {
+  const style = map.getStyle();
+  const layers = style && style.layers ? style.layers : [];
+  const uncapped = [];
+  for (const layer of layers) {
+    const min = typeof layer.minzoom === "number" ? layer.minzoom : 0;
+    // If a layer has maxzoom <= 15 (common), raise it to 24
+    if (typeof layer.maxzoom === "number" && layer.maxzoom < 24) {
+      map.setLayerZoomRange(layer.id, min, 24);
+      uncapped.push({ id: layer.id, oldMax: layer.maxzoom });
+    }
+  }
+  if (uncapped.length) {
+    console.log("Uncapped layer maxzoom to 24:", uncapped);
+  }
+});
+
 map.on("load", () => {
   console.log("map: load");
   console.log("Current zoom:", map.getZoom());
-  console.log("Current center:", map.getCenter());
-  console.log("Current bounds:", map.getBounds());
 });
 
-map.on("styledata", () => console.log("map: styledata"));
 map.on("error", (e) => console.error("Map error:", e?.error || e));
-
-map.on("dataloading", (e) => console.log("Data loading:", e.dataType));
-map.on("data", (e) => console.log("Data loaded:", e.dataType, e.sourceDataType));
-map.on("sourcedata", (e) => {
-  console.log("Source data:", e.dataType, e.sourceId);
-});
